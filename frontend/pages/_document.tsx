28102ef5452a208849a5876629a958ca33b70aa0
nelson frank <nelsonfr78@outlook.com>
2022-04-27 12:35:02 +0300

    fix: Update _document.tsx file
    - Add font links that specifies font weight from google fonts

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -4,10 +4,9 @@ export default function Document() {
     return (
         <Html>
             <Head>
-                <link
-                    href="https://fonts.googleapis.com/css2?family=Inter&display=optional"
-                    rel="stylesheet"
-                />
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+                <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;300;400;500;600;700&display=swap" rel="stylesheet" />
             </Head>
             <body>
                 <Main />
