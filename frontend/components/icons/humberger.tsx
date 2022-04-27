6609937019ab6cb9f5fb69bf8438c778a0dadf1e
nelson frank <nelsonfr78@outlook.com>
2022-04-27 11:50:10 +0300

    feat: Add humberger icon in icon component collection

---
+++ frontend/components/icons/humberger.tsx
@@ -0,0 +1,21 @@
+
+const Humberger = () => {
+    return (
+        <>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                x="0px"
+                y="0px"
+                height='32'
+                width='32'
+                viewBox="0 0 32 32"
+            >
+                <rect x="0" y="3" fill="#000000" width="32" height="5" rx="3" ry="3" />
+                <rect x="0" y="14" fill="#000000" width="32" height="5" rx="3" ry="3" />
+                <rect x="0" y="25" fill="#000000" width="32" height="5" rx="3" ry="3" />
+            </svg>
+        </>
+    );
+};
+
+export default Humberger;
--- frontend/components/icons/index.ts
+++ frontend/components/icons/index.ts
@@ -1,1 +1,3 @@
 export { default as Close } from "./close";
+
+export { default as Humberger } from "./humberger";
