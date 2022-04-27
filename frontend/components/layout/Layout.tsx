f2f698feca283cf19b6c46754ad1f0b4617c1f98
nelson frank <nelsonfr78@outlook.com>
2022-04-27 12:55:51 +0300

    feat: Add layout wrapper component

---
+++ frontend/components/layout/Layout.tsx
@@ -0,0 +1,15 @@
+import Navbar from '@/ui/navbar'
+import React, { ReactNode } from 'react'
+
+const Layout = ({ children }: React.PropsWithChildren<{}>): JSX.Element => {
+    return (
+        <>
+            <Navbar />
+            <section className='nf-max-w-screen-xl nf-mx-auto nf-px-4 nf-my-4'>
+                {children}
+            </section>
+        </>
+    )
+}
+
+export default Layout
