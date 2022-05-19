868c02c814e276a0078df80f56d17f2f8509f83d
nelson frank <nelsonfr78@outlook.com>
2022-05-19 04:05:33 +0300

    fix: Update Experience section ui with Experience-title component

--- frontend/ui/experience/Experience.tsx
+++ frontend/ui/experience/Experience.tsx
@@ -1,13 +1,56 @@
-import React from 'react'
+import ExperienceTitleCard, { ExperinceTitleCardProps } from '@/components/experience-title-card/ExperinceTitleCard';
 
-const Experience = () => {
+const WorkExperince: ExperinceTitleCardProps[] = [
+    {
+        title: "Full-stack engineer",
+        organizationName: "Sndbx by id8",
+        location: "Dar es salaam",
+        duration: {
+            from: new Date("Tuesday, December 19, 2021 12:11:01 AM"),
+            to: "Present",
+        },
+    },
+    {
+        title: "React Developer",
+        organizationName: "Databalk",
+        location: "Netherlands",
+        duration: {
+            from: new Date("Tuesday, October 19, 2021 12:11:01 AM"),
+            to: new Date("Monday, January 19, 2022 12:11:01 AM"),
+        },
+    },
+    {
+        title: "Javascript full-stack engineer",
+        organizationName: "Nipale",
+        location: "Dar es salaam",
+        duration: {
+            from: new Date("Tuesday, January 19, 2021 12:11:01 AM"),
+            to: new Date("Monday, August 19, 2021 12:11:01 AM"),
+        },
+    },
+    {
+        title: "Front-end engineer",
+        organizationName: "Id8 Space",
+        location: "Dar es salaam",
+        duration: {
+            from: new Date("Tuesday, May 19, 2020 12:11:01 AM"),
+            to: new Date("Monday, January 19, 2021 12:11:01 AM"),
+        },
+    },
+]; const Experience = () => {
     return (
-        <div className="nf-flex nf-items-center nf-mt-4">
-            <div className="nf-w-3 nf-h-3 nf-border-2 nf-border-primary nf-rounded-full nf-mr-4 md:nf-mr-8 nf-animate-bounce"></div>
+        <div>
+            <h1 className="nf-text-3xl nf-font-semibold nf-text-primary">Work experince</h1>
             <div>
-                <span className="nf-text-xs nf-text-gray"> July 2017 - Present <span className="nf-mx-1">&#183;</span>Dar es salaam</span>
-                <h2 className="nf-text-lg nf-font-semibold">Full-stack web developer</h2>
-                <h3 className="nf-text-sm nf-text-gray nf-font-medium">Id8 Space</h3>
+                {
+                    WorkExperince.map((item, index) => <ExperienceTitleCard
+                        title={item.title}
+                        organizationName={item.organizationName}
+                        duration={item.duration}
+                        location={item.location}
+                        key={item.title + index}
+                    />)
+                }
             </div>
         </div>
     )
