921fc94e963ea1d872e93564af1df11f1a894a39
nelson frank <nelsonfr78@outlook.com>
2022-08-29 23:06:39 +0300

    fix: Reduce empty space around header in small screen

--- frontend/ui/navbar/navbar.tsx
+++ frontend/ui/navbar/navbar.tsx
@@ -13,45 +13,53 @@ const Navbar = () => {
         setShowSidebar(!showSidebar)
     }
     return (
-        <header>
-            <section className={`${showSidebar && 'nf-hidden'} nf-max-w-7xl nf-mx-auto nf-my-8 nf-p-4`}>
-                <div className="nf-flex nf-justify-between nf-items-center">
-                    <div>
-                        <Link href="/">
-                            <Image src={Logo} alt="Logo svg" width="60" height="60" />
-                        </Link>
-                    </div>
-                    <div className="nf-hidden md:nf-inline-block md:nf-w-3/5 lg:nf-w-2/5">
-                        <ul className="nf-flex nf-items-center nf-justify-evenly">
-                            <li>
-                                <button className="nf-text-xl focus:nf-text-primary">Blog</button>
-                            </li>
-                            <li>
-                                <button className="nf-text-xl focus:nf-text-primary">
-                                    Projects
-                                </button>
-                            </li>
-                            <li>
-                                <button className="nf-text-xl focus:nf-text-primary">Uses</button>
-                            </li>
-                            <li>
-                                <Link href="/about">
-                                    <button className="nf-text-xl focus:nf-text-primary">
-                                        About
-                                    </button>
-                                </Link>
-                            </li>
-                        </ul>
-                    </div>
-                    <div className="nf-block md:nf-hidden">
-                        <button onClick={handleToggleSidebar}>
-                            <Humberger />
-                        </button>
-                    </div>
-                </div>
-            </section>
-            {showSidebar && <Sidebar handleToggleSidebar={handleToggleSidebar} />}
-        </header>
+      <header>
+        <section
+          className={`${
+            showSidebar && "nf-hidden"
+          } nf-max-w-7xl nf-mx-auto nf-my-4 md:nf-my-8 nf-p-4`}
+        >
+          <div className="nf-flex nf-justify-between nf-items-center">
+            <div>
+              <Link href="/">
+                <Image src={Logo} alt="Logo svg" width="60" height="60" />
+              </Link>
+            </div>
+            <div className="nf-hidden md:nf-inline-block md:nf-w-3/5 lg:nf-w-2/5">
+              <ul className="nf-flex nf-items-center nf-justify-evenly">
+                <li>
+                  <button className="nf-text-xl focus:nf-text-primary">
+                    Blog
+                  </button>
+                </li>
+                <li>
+                  <button className="nf-text-xl focus:nf-text-primary">
+                    Projects
+                  </button>
+                </li>
+                <li>
+                  <button className="nf-text-xl focus:nf-text-primary">
+                    Uses
+                  </button>
+                </li>
+                <li>
+                  <Link href="/about">
+                    <button className="nf-text-xl focus:nf-text-primary">
+                      About
+                    </button>
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <div className="nf-block md:nf-hidden">
+              <button onClick={handleToggleSidebar}>
+                <Humberger />
+              </button>
+            </div>
+          </div>
+        </section>
+        {showSidebar && <Sidebar handleToggleSidebar={handleToggleSidebar} />}
+      </header>
     );
 };
 
