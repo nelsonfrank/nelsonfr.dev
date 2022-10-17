402b0c1c64f6c2dcabaae7b9ca83b83c62866aac
nelson frank <nelsonfr78@outlook.com>
2022-10-17 12:33:31 +0300

    fix: Improve layout

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -6,7 +6,7 @@ const Layout = ({ children }: React.PropsWithChildren<{}>): JSX.Element => {
   return (
     <section className="nf-flex nf-flex-col nf-min-h-screen">
       <Navbar />
-      <section>{children}</section>
+      <section className="nf-flex-1">{children}</section>
 
       <Footer />
     </section>
