54a67912c107046f1c9198f417967747532df08d
nelson frank <nelsonfr78@outlook.com>
2022-11-26 17:18:40 +0300

    fix: Add email to contact page

--- pages/contacts.tsx
+++ pages/contacts.tsx
@@ -1,4 +1,5 @@
 import Github from "@/components/icons/github";
+import Gmail from "@/components/icons/gmail";
 import Twitter from "@/components/icons/twitter";
 import React from "react";
 
@@ -8,31 +9,32 @@ const Contacts = () => {
       <div className="nf-mb-4">
         <h1 className="nf-font-medium nf-text-3xl">Contacts</h1>
       </div>
-      <div className="nf-flex nf-gap-2 nf-items-center nf-my-4">
-        <Github height={40} width={40} />
-        <a href="https://github.com/nelsonfrank" target="_blank">
-          <span className="nf-text-primary nf-text-xl nf-font-medium">
-            Nelsonfrank
-          </span>
-        </a>
+      <div className="nf-px-4">
+        <div className="nf-flex nf-gap-2 nf-items-center nf-my-4">
+          <Github height={40} width={40} />
+          <a href="https://github.com/nelsonfrank" target="_blank">
+            <span className="nf-text-primary nf-text-xl nf-font-medium">
+              Nelsonfrank
+            </span>
+          </a>
+        </div>
+        <div className="nf-flex nf-gap-2 nf-items-center nf-my-4">
+          <Twitter height={40} width={40} />
+          <a href="https://twitter.com/nelsonfr_" target="_blank">
+            <span className="nf-text-primary nf-text-xl nf-font-medium">
+              Nelsonfr_
+            </span>
+          </a>
+        </div>
+        <div className="nf-flex nf-gap-2 nf-items-center nf-my-4">
+          <Gmail height={40} width={40} />
+          <a href="mailto:nelsonfrank741@gmail.com" target="_blank">
+            <span className="nf-text-primary nf-text-xl nf-font-medium">
+              nelsonfrank741@gmail.com
+            </span>
+          </a>
+        </div>
       </div>
-      <div className="nf-flex nf-gap-2 nf-items-center nf-my-4">
-        <Twitter height={40} width={40} />
-        <a href="https://twitter.com/nelsonfr_" target="_blank">
-          <span className="nf-text-primary nf-text-xl nf-font-medium">
-            Nelsonfr_
-          </span>
-        </a>
-      </div>
-      {/* <div className="nf-flex nf-gap-2 nf-items-center nf-my-4">
-         Replace email with Gmail Icon 
-        <h1>Email:</h1>
-        <a href="https://twitter.com/nelsonfr_" target="_blank">
-          <span className="nf-text-primary nf-text-xl nf-font-medium">
-            nelsonfrank741@gmail.com
-          </span>
-        </a>
-      </div> */}
     </div>
   );
 };
