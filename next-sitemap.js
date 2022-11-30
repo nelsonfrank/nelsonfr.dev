331fa965ffbbccd4e598394c7f39ef6e03d8fb59
nelson frank <nelsonfr78@outlook.com>
2022-11-30 12:39:03 +0300

    feat: Add site map configuration

---
+++ next-sitemap.js
@@ -0,0 +1,9 @@
+const siteUrl = "https://nelsonfr.dev";
+
+module.exports = {
+  siteUrl,
+  generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [{ userAgent: "*", allow: "/" }],
+  },
+};
