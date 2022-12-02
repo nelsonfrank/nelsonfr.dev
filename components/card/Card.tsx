989bf6bd8999bb4b5cce23b4d3db867cda8ec589
nelson frank <nelsonfr78@outlook.com>
2022-12-02 11:33:39 +0300

    feat: Add fallback image in Card

--- components/card/Card.tsx
+++ components/card/Card.tsx
@@ -1,22 +1,40 @@
-const Card = () => {
+import { useState } from "react";
+
+import fallbackImage from "@/public/fallback.jpg";
+
+interface CardProps {
+  title: string;
+  description?: string;
+  img?: string;
+  fallbackImg?: string;
+}
+
+const Card = ({
+  title,
+  description,
+  img,
+  fallbackImg = "/fallback.jpg",
+}: CardProps) => {
+  const [error, setError] = useState<React.SyntheticEvent<
+    HTMLImageElement,
+    Event
+  > | null>(null);
   return (
     <div className="nf-max-w-sm nf-mx-4 nf-my-4 nf-bg-white nf-rounded-lg nf-border nf-border-gray nf-shadow-md">
       <a href="#">
         <img
           className="nf-rounded-t-lg"
-          src="https://images.unsplash.com/photo-1662368357622-d840d7f5ab60?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-          alt="project card"
+          src={error ? fallbackImg : img}
+          alt={description}
+          onError={setError}
         />
       </a>
       <div className="nf-py-5 nf-px-4">
-        <a href="#">
-          <h5 className="nf-my-2 nf-text-xl nf-font-bold nf-tracking-normal nf-text-gray-900">
-            Noteworthy technology acquisitions 2021
-          </h5>
-        </a>
+        <h5 className="nf-my-2 nf-text-xl nf-font-bold nf-tracking-normal nf-text-gray-900">
+          {title}
+        </h5>
         <p className="nf-mb-3 nf-font-normal nf-text-left nf-text-gray-700">
-          Here are the biggest enterprise technology acquisitions of 2021 so
-          far, in reverse chronological order.
+          {description}
         </p>
       </div>
     </div>
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -2,22 +2,21 @@
 import Card from "@/components/card";
 import SEO from "@/components/seo";
 
+import ProjectsData from "@/data/projects.json";
+
 const Projects = () => {
   return (
     <div className="nf-grid nf-grid-cols-card-lists nf-gap-2 nf-justify-items-center">
       <SEO title="Projects" />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      {ProjectsData.map((project) => (
+        <Card
+          key={project.name}
+          title={project.name}
+          description={project.description}
+          img={project.img}
+          fallbackImg={project.fallbackImg}
+        />
+      ))}
     </div>
   );
 };
