8b79fc17ca64e8c6530dec1e90f97d6608525ff1
NelsonFrank <nelsonfr78@outlook.com>
2023-10-14 01:05:25 +0300

    feat: Add next-plausible plugin and configure

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 // dependencies
 import type { AppProps } from "next/app";
+import PlausibleProvider from "next-plausible";
 
 // components
 import Layout from "@/components/layout";
@@ -9,10 +10,12 @@ import "../styles/globals.css";
 
 function Root({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  );
+		<PlausibleProvider domain='example.com'>
+			<Layout>
+				<Component {...pageProps} />
+			</Layout>
+		</PlausibleProvider>
+	);
 }
 
 export default Root;
