262ab64bb47ec0f1c29c472723adc6622733841a
NelsonFrank <nelsonfr78@outlook.com>
2023-10-14 00:51:47 +0300

    feat: Add js snippet for plausible analytic tool

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,25 +2,30 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
     return (
-      <Html lang="en">
-        <Head>
-          <link rel="shortcut icon" href="/favicon.ico" />
+			<Html lang='en'>
+				<Head>
+					<link rel='shortcut icon' href='/favicon.ico' />
 
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossOrigin=""
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Inter:wght@100;300;400;500;600;700&display=swap"
-            rel="stylesheet"
-          />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
+					<link rel='preconnect' href='https://fonts.googleapis.com' />
+					<link
+						rel='preconnect'
+						href='https://fonts.gstatic.com'
+						crossOrigin=''
+					/>
+					<link
+						href='https://fonts.googleapis.com/css2?family=Inter:wght@100;300;400;500;600;700&display=swap'
+						rel='stylesheet'
+					/>
+					<script
+						defer
+						data-domain='nelsonfr.dev'
+						src='https://plausible.io/js/script.js'
+					></script>
+				</Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
 }
