96791bb039207e9c361090fc65f50b41506bf740
NelsonFrank <nelsonfr78@outlook.com>
2023-10-17 16:35:45 +0300

    feat: Moving tags page to app directory

--- app/tags/[tag]/page.tsx
+++ app/tags/[tag]/page.tsx
@@ -9,7 +9,7 @@ import SEO from "@/components/seo";
 import { getAllPosts, PostMeta } from "@/utils/blog";
 
 const Tags = ({ tag, posts }: { tag: string; posts: PostMeta[] }) => {
-  return (
+	return (
 		<div>
 			<SEO title={`Tag: ${tag}`} />
 			<h1 className='text-4xl font-semibold capitalize'>Tag:{tag}</h1>
@@ -21,24 +21,24 @@ const Tags = ({ tag, posts }: { tag: string; posts: PostMeta[] }) => {
 };
 
 export const getStaticProps: GetStaticProps = ({ params }) => {
-  const { tag } = params as { tag: string };
-  const posts = getAllPosts().filter((post) => post.meta.tags.includes(tag));
-  return {
-    props: {
-      tag,
-      posts: posts.map((post) => post.meta),
-    },
-  };
+	const { tag } = params as { tag: string };
+	const posts = getAllPosts().filter((post) => post.meta.tags.includes(tag));
+	return {
+		props: {
+			tag,
+			posts: posts.map((post) => post.meta),
+		},
+	};
 };
 
 export const getStaticPaths: GetStaticPaths = () => {
-  const posts = getAllPosts();
-  const tags = new Set(posts.map((post) => post.meta.tags).flat());
-  const paths = Array.from(tags).map((tag) => ({ params: { tag } }));
+	const posts = getAllPosts();
+	const tags = new Set(posts.map((post) => post.meta.tags).flat());
+	const paths = Array.from(tags).map((tag) => ({ params: { tag } }));
 
-  return {
-    paths,
-    fallback: false,
-  };
+	return {
+		paths,
+		fallback: false,
+	};
 };
 export default Tags;
