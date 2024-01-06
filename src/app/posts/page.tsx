a71cdc1b443f8f679a93616e62fc94fb6fdb0da8
NelsonFrank <nelsonfr78@outlook.com>
2024-01-06 20:22:28 +0300

    style: :lipstick: improve the style of the post card

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -4,28 +4,33 @@ import { Metadata } from "next";
 
 // utils
 import { getAllPosts, PostMeta } from "@/libs/mdx";
+import { PostCard } from "../posts";
 
 export const metadata: Metadata = {
-	title: "Posts",
-	description:
-		"I writes and posts articles based on web development regularly, check them out",
+  title: "Posts",
+  description:
+    "I writes and posts articles based on web development regularly, check them out",
 };
 
 const Blog = () => {
-	const posts: PostMeta[] = getAllPosts()
-		.slice(0, 9)
-		.reverse()
-		.map((post) => post.meta);
-	return (
-		<div>
-			<div className='my-4'>
-				<h1 className='text-4xl font-semibold'>Posts</h1>
-				<div>
-					<Posts posts={posts} />
-				</div>
-			</div>
-		</div>
-	);
+  const posts: PostMeta[] = getAllPosts()
+    .slice(0, 9)
+    .reverse()
+    .map((post) => post.meta);
+  return (
+    <div>
+      <div className="my-8">
+        <h1 className="text-4xl font-semibold">Posts</h1>
+        <section className="grid grid-cols-1 gap-6 py-8 md:grid-cols-2 lg:grid-cols-3">
+          {posts.map((post) => (
+            <div className="col-span-1" key={post.slug}>
+              <PostCard post={post} />
+            </div>
+          ))}
+        </section>
+      </div>
+    </div>
+  );
 };
 
 export default Blog;
