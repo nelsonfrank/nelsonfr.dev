91fcfa24914c33196410328a4b75361133f13404
NelsonFrank <nelsonfr78@outlook.com>
2024-01-07 16:55:58 +0300

    feat: :sparkles: add tags in posts card and tag filter functionality

--- src/app/posts.tsx
+++ src/app/posts.tsx
@@ -1,40 +1,46 @@
-
+import { Badge } from "@/components/ui/badge";
 import { getAllPosts, PostMeta } from "@/libs/mdx";
+import { cn, randomizeBadgeColor } from "@/libs/utils";
 import Link from "next/link";
 
 export const PostCard = ({ post }: { post: PostMeta }) => (
-  <Link
-    href={`/posts/${post.slug}`}
-    className="relative top-0 block h-full overflow-hidden rounded-xl bg-white shadow-lg transition-all duration-75 ease-in-out hover:-top-2 hover:shadow-xl"
-  >
-    <img
-      className="squiggle card-img"
-      src={post.image}
-      alt={post.title}
-      width="360"
-      height="192"
-    />
-    <div className="px-8 py-6">
+  <div className="relative top-0 flex h-full flex-col overflow-hidden rounded-xl bg-white shadow-lg transition-all duration-75 ease-in-out hover:-top-2 hover:shadow-xl">
+    <Link href={`/posts/${post.slug}`}>
+      <img
+        className="squiggle card-img"
+        src={post.image}
+        alt={post.title}
+        width="360"
+        height="192"
+      />
+    </Link>
+    <div className="relative flex-1 px-8 py-6">
       <h2 className="text-2xl font-bold capitalize leading-tight text-black">
-        {post.title.toLowerCase()}
+        <Link href={`/posts/${post.slug}`}>{post.title.toLowerCase()}</Link>
       </h2>
-      <p className="mt-4 flex items-center text-xs text-gray-600">
+      <p className="my-4 flex items-center text-xs text-gray-600">
         {new Date(post.date).toLocaleDateString()}
       </p>
+      <div className="absolute bottom-1 flex gap-2">
+        {post.tags.slice(0, 3).map((tag) => (
+          <Link key={tag} href={`/tags/${tag}`}>
+            <Badge className={cn(randomizeBadgeColor())}>{tag}</Badge>
+          </Link>
+        ))}
+      </div>
     </div>
-  </Link>
+  </div>
 );
 
-
 const Posts = () => {
-    const posts: PostMeta[] = getAllPosts()
-		.slice(0, 3)
-		.reverse()
-		.map((post) => post.meta)
+  const posts: PostMeta[] = getAllPosts()
+    .slice(0, 3)
+    .reverse()
+    .map((post) => post.meta);
   return (
     <section className="grid grid-cols-1 gap-6 py-8 md:grid-cols-2 lg:grid-cols-3">
       {posts.map((post, index) => (
-        <div className="col-span-1" key={index+1}>
+        <div className="col-span-1" key={index + 1}>
           <PostCard post={post} />
         </div>
       ))}
--- src/libs/utils.ts
+++ src/libs/utils.ts
@@ -5,3 +5,20 @@ import { clsx, type ClassValue } from "clsx"
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs))
 }
+
+export function randomizeBadgeColor() {
+    const backgroundColorClasses = [
+        'bg-blue-100 text-blue-800',
+        'bg-gray-100 text-gray-800',
+        'bg-red-100 text-red-800',
+        'bg-green-100 text-green-800',
+        'bg-yellow-100 text-yellow-800',
+        'bg-indigo-100 text-indigo-800',
+        'bg-purple-100 text-purple-800',
+        'bg-pink-100 text-pink-800',
+    ];
+
+    const randomIndex = Math.floor(Math.random() * backgroundColorClasses.length);
+
+   return backgroundColorClasses[randomIndex];
+}
