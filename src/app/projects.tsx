c963bb503eed375aafbdcbe87f6496a193e6fe3d
NelsonFrank <nelsonfr78@outlook.com>
2024-01-11 10:28:11 +0300

    feat: :sparkles: pass real data fromjson file to project card in landing page

--- src/app/projects.tsx
+++ src/app/projects.tsx
@@ -1,46 +1,21 @@
 import { ProjectCard } from "@/components/card/project-card";
 
-export interface IProject {
-  date: string;
-  readTime: string;
-  title: string;
-  image: string;
-}
-const projects: IProject[] = [
-  {
-    date: "12/29/2022",
-    readTime: "5 mins",
-    title: "INTRODUCING ASTRO: SHIP LESS JAVASCRIPT",
-    image:
-      "https://images.unsplash.com/photo-1585020430145-2a6b034f7729?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
-  {
-    date: "12/29/2022",
-    readTime: "5 mins",
-    title: "INTRODUCING ASTRO: SHIP LESS JAVASCRIPT",
-    image:
-      "https://images.unsplash.com/photo-1585020430145-2a6b034f7729?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
-  {
-    date: "12/29/2022",
-    readTime: "5 mins",
-    title: "INTRODUCING ASTRO: SHIP LESS JAVASCRIPT",
-    image:
-      "https://images.unsplash.com/photo-1585020430145-2a6b034f7729?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  }
-];
+import projects from "@/content/data/projects.json";
 
 
-const Projects = () => {
+
+
+
+const Page = () => {
   return (
-    <section className="grid grid-cols-1 gap-6 py-8 md:grid-cols-2 lg:grid-cols-3">
-      {projects.map((project, index) => (
+    <section className="grid grid-cols-1 gap-6 py-8 md:grid-cols-2">
+      {projects.slice(0,3).map((project, index) => (
         <div className="col-span-1" key={index+1}>
-          <ProjectCard  />
+          <ProjectCard project={project} />
         </div>
       ))}
     </section>
   );
 };
 
-export default Projects;
+export default Page;
